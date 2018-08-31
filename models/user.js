dd227ff111008cd23b8529c1419f934a79d10ae7
Alan <a386226@gmail.com>
2018-08-31 10:26:26 -0700

    updated user schema

--- models/user.js
+++ models/user.js
@@ -2,10 +2,25 @@ const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-name: String,
-email: String,
-password: String
-});
+
+    name: {
+        type: String,
+        required: true},
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
+    password: { type: String, required: true , select: false}
+
+})
+
+userSchema.set('toJSON', {
+  transform: function(doc, ret, opt) {
+      delete ret['password']
+      return ret
+  }
+})
 
 const User = mongoose.model('User', userSchema);
 
