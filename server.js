6eb8b5801a64f13a72f42f8920cfbcdf94515208
Alan <a386226@gmail.com>
2018-09-01 14:44:37 -0700

    fixed modal on landing page, started modal on map page, etc

--- server.js
+++ server.js
@@ -171,48 +171,6 @@ app.get('/api/legacy', (req, res) => {
     });
   });
 
-// protected route - a route only a user with a jwt token in their header can access.
-// app.post('/api/posts', verifyToken, (req, res) => {
-//   console.log(req.token)
-//   jwt.verify(req.token, 'waffles', (err, authData) => {
-//     if(err) {
-//       res.sendStatus(403);
-//     } else {
-//       res.json({
-//         message: 'Post created',
-//         authData
-//       });
-//     }
-
-//   });
-// });
-
-// FORMAT OF TOKEN
-// Authorization: Bearer <access_token>
-
-// // Verify Tokenj 
-// function verifyToken(req, res, next) {
-//   console.log("in verify...");
-//   // Get auth header value
-//   // when we send our token, we want to send it in our header
-//   const bearerHeader = req.headers['authorization'];
-//   console.log(bearerHeader)
-//   // Check if bearer is undefined
-//   if(typeof bearerHeader !== 'undefined'){
-//     const bearer = bearerHeader.split(' ');
-//     // Get token from array
-//     const bearerToken = bearer[1];
-//     // Set the token
-//     req.token = bearerToken;
-//     // Next middleware
-//     next();
-
-//   } else {
-//     // Forbidden
-//     res.sendStatus(403);
-//   }
-// }
-
 
 // Server
 app.listen(process.env.PORT || 3000, () => {
