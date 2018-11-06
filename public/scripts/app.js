54bc60d58744dd43d8268bc215db08026e9fc517
natalie-poulson <natalieplsn@gmail.com>
2018-11-05 18:48:31 -0800

    changed to es6 in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,75 +1,42 @@
 $('#formLogin'). on ('submit', (e) => {
     e.preventDefault();
-
-    console.log($('#formLogin').serialize())
     $.ajax({
         method: 'POST',
         url:'/api/login',
         data: $('#formLogin').serialize(),
-        success:loginSuccess,
-        error: loginError
+        success: (json) => {
+            $('main p').first('.error-message').fadeOut();
+            window.location.pathname = '/map'
+            },
+        error: (json) =>  {
+            $('main p').first('.error-message').fadeOut();
+            $('main p').first('.error-message').fadeIn();
+            $('main p').css('display', 'flex');
+        }
     });
 })
 
 $('#formSignUp'). on ('submit', (e) => {
     e.preventDefault();
-
     $.ajax({
         method: 'POST',
         url:'/api/signup',
         data:$('#formSignUp').serialize(),
-        success:signUpSuccess,
-        error: signUpError
+        success: (json) => {
+            $('.modal-body .error-message').fadeOut(500);
+            setTimeout(function() { alert("User created. Thanks for creating an account with us."); }, 1000);
+            setTimeout(function () {window.location.pathname = '/map';}, 1500);
+        },
+        error: (json) => {
+            if (json.status === 409){
+                $('#duplicateMessage').fadeOut();
+                $('#duplicateMessage').fadeIn();
+            }
+        }
     });
 });
 
-function signUpError (json) {
-    $('#formSignUp input').each(function () {
-    if ( $(this).val().length === 0) {
-        $('#duplicateMessage').fadeOut();
-        $(this).siblings().fadeIn(1000);
-        return;
-    }
-    if ( $(this).val().length !== 0) {
-        $('#duplicateMessage').fadeOut();
-        $(this).siblings().fadeOut(200);
-        return;
-    }
-});
-    if (json.status === 500){
-        $('#duplicateMessage').fadeOut();
-        $('#emailValidation').fadeIn().text('Please enter a valid email address.');
-    }
-    if (json.status === 409){
-        $('#duplicateMessage').fadeOut();
-        $('#duplicateMessage').fadeIn();
-    }
-};
-
-function loginSuccess (json) {
-    $('main p').first('.error-message').fadeOut();
-    window.location.pathname = '/map'
-}   
-
-function loginError (json) {
-    console.log(json)
-    $('main p').first('.error-message').fadeOut();
-    $('main p').first('.error-message').fadeIn();
-    $('main p').css('display', 'flex');
-}
-
-function signUpSuccess (json) {
-    console.log(json);
-    $('.modal-body .error-message').fadeOut(500);
-    setTimeout(function() { alert("User created. Thanks for creating an account with us."); }, 1000);
-    setTimeout(function () {window.location.pathname = '/map';}, 1500);
-}
-
-function handleError(e) {
-    console.log('error', e);
-};
-
-$('#logout').on ('click', function () {
+$('#logout').on ('click', () => {
     window.location.pathname = '/'
 })
 
